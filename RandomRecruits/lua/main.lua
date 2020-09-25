b15c71126b553d7dbd7f95545357a312a858f2f1
Vasili Novikov <n1m5spm@yandex.com>
2020-09-25 14:59:18 +0200

    Generate units at keep

--- RandomRecruits/lua/main.lua
+++ RandomRecruits/lua/main.lua
@@ -23,10 +23,9 @@ local era_set = {}
 
 local function init_era()
 	for multiplayer_side in helper.child_range(wesnoth.game_config.era, "multiplayer_side") do
-		local units = multiplayer_side.recruit or ""
+		local units = multiplayer_side.recruit or multiplayer_side.leader or ""
 		for _, unit in ipairs(split_comma(units)) do
 			if era_set[unit] == nil and wesnoth.unit_types[unit] then
-				-- print("importing era unit " .. unit)
 				era_set[unit] = true
 				era_array[#era_array + 1] = unit
 			end
@@ -46,22 +45,40 @@ local function random_recruit()
 	return era_array[helper.rand(era_unit_rand_string)]
 end
 
+local function find_leader(side)
+	for _, leader in ipairs(wesnoth.get_units { canrecruit = true, side = side.side }) do
+		if wesnoth.get_terrain_info(wesnoth.get_terrain(leader.x, leader.y)).keep then
+			return leader
+		end
+	end
+end
 
-on_event("turn refresh", function()
-	local side = wesnoth.sides[wesnoth.current.side]
+local function generate_units(side)
+	side = side.side and side or wesnoth.sides[wesnoth.current.side]
 	side.recruit = {};
-	local start_loc = wesnoth.get_starting_location(side.side)
+	local leader = find_leader(side)
+	if leader == nil then
+		return
+	end
 	while true do
 		local recruit_type = random_recruit()
 		if side.gold >= wesnoth.unit_types[recruit_type].cost then
 			local unit = wesnoth.create_unit { type = recruit_type, side = side.side }
-			local x, y = wesnoth.find_vacant_tile(start_loc[1], start_loc[2], unit)
+			local x, y = wesnoth.find_vacant_tile(leader.x, leader.y, unit)
 			wesnoth.put_unit(unit, x, y)
 			side.gold = side.gold - wesnoth.unit_types[recruit_type].cost
 		else
 			break
 		end
 	end
+end
+
+on_event("start", function()
+	for _side_number, side in ipairs(wesnoth.sides) do
+		generate_units(side)
+	end
 end)
+
+on_event("turn refresh", generate_units)
 
 -- >>
